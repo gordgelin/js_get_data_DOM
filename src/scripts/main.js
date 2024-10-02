057ff54f70d40fb6f35a0b93801db3e9d99f9aae
gordgelin <gordgelin@s69a.su>
2024-10-02 05:02:35 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const spans = document.querySelectorAll("[class='population']");
+
+let sum = 0;
+
+for (const span of spans) {
+  sum += Number(span.textContent.replaceAll(',', ''));
+}
+
+document.querySelector("[class='total']").innerHTML =
+  'Total: ' + sum.toLocaleString('en-US');
+
+document.querySelector("[class='average']").innerHTML =
+  'Average: ' + Math.round(sum / spans.length).toLocaleString('en-US');
